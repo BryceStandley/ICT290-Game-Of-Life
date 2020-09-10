4bb7e97c431405dc0d93d615196e8c1e414d8b71
Bryce Standley <bryce@vectorpixel.net>
2020-09-10 12:19:58 +0800

    Some rendering
    -Made rendering a 2D array of random cells
    -Some positioning to be fixed
    
    -To add main CA code to implement real game of life

--- ICT290-Game-Of-Life/main.cpp
+++ ICT290-Game-Of-Life/main.cpp
@@ -15,88 +15,243 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <random>
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+#define WHITE 1,1,1
+#define BLACK 0,0,0
+#define ScreenWidth 800
+#define ScreenHeight 800
+#define LOG(foo, x) std::cout << foo << x << std::endl
+typedef unsigned char pixel;
+
 void display(void);
 void reshape(int, int);
-void readConfig(void);
+bool readConfig(void);
+void elementryCA(void);
+void drawCA(void);
+void init(void);
+void generatePixels(int, int, int, int);
 
 std::string configFileName = "gol_cfg.txt";
 int gridSizeSquare;
+bool debugMode = true;
+
+const int col = 10;
+const int row = 10;
+int grid[col][row];
+pixel image_buffer[col * row];
+
 
 
 int main(int argc, char** argv)
 {
     //Note config must be placed in the same file as exe and wont file config file in debug mode
-    readConfig();
+    //if (readConfig())
+    //{    
+    //}
+
+    elementryCA();
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_RGB);
 
-    glutInitWindowSize(800, 600); // 800x600 window
+    glutInitWindowSize(ScreenWidth, ScreenHeight); // 800x600 window
     glutInitWindowPosition(0, 0);
+
     glutCreateWindow("Conway's Game Of Life - OpenGL - ICT290 - Bryce Standley");
 
-    glutDisplayFunc(display);
-    glutIdleFunc(display);
-    glutReshapeFunc(reshape);
 
-    //More Glut Func settings go here
+    init();
 
     glutMainLoop();
 
     return 0;
 }
 
+void init()
+{
+    
+   
+    
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    
+
+    
+    /*
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(ScreenWidth, ScreenHeight);
+    glutCreateWindow("Conway's Game Of Life - OpenGL - ICT290 - Bryce Standley");
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glutDisplayFunc(display);
+    //glutTimerFunc(1000 / 60, timer, 0);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0f, (float)ScreenWidth, 0.0f, (float)ScreenHeight);
+    glTranslatef(0, 0, -20);
+    */
+}
 
 void display()
 {
 
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    //glTranslatef(0, 0, -10);
+
+    drawCA();
+/*
+    GLdouble x = -1;
+    GLdouble y = 1;
+    GLdouble yPos = 1.0 / row;
+    GLdouble xPos = 1.0 / col;
+
+    //std::cout << "xPos: " << (double)xPos << std::endl;
+    
+    //glColor3f(1, 1, 1);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(x, y, 0);
+    glVertex3f(x, y - yPos, 0);
+    glVertex3f(x + xPos, y - yPos, 0);
+
+    glVertex3f(x, y, 0);
+    glVertex3f(x + xPos, y - yPos, 0);
+    glVertex3f(x + xPos, y, 0);
+    glEnd();
+    
+    glBegin(GL_TRIANGLES); // Triangles
+    glVertex2d(x, y); // top left 1 0,0
+
+    glVertex2d(x, y - yPos); //bottom left 2 0,-1
+    glVertex2d(x + xPos, y - yPos); //bottom right 3 1,-1
+
+    glVertex2d(x, y); // top left 1 0,0
+    glVertex2d(x + xPos, y - yPos); //bottom right 3 1,-1
+    glVertex2d(x + xPos, y); //top left 4 1,0
+    glEnd();
+    */
+
+    //glDrawPixels(col, row, GL_LUMINANCE, GL_UNSIGNED_BYTE, image_buffer);
+    glFlush();
 }
 
-void reshape(int width, int height)
+
+bool readConfig()
 {
-    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 1, 0, 1);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glutPostRedisplay();
-
-
-}
-
-void readConfig()
-{
-    std::ifstream configFile;
-    configFile.open(configFileName);
-
-    if (!configFile.is_open())
+    if (!debugMode)
     {
-        std::cout << "Error when opening config file " << configFileName << " Does the file exist? " << "'" << configFile.eof() << "'" << std::endl;
-    }
+        std::ifstream configFile;
+        configFile.open(configFileName);
 
-    while (configFile)
-    {
-        std::string line;
-        std::getline(configFile, line, ':');
-
-        configFile >> gridSizeSquare;
-
-        configFile >> std::ws;
-
-        if (!configFile)
+        if (!configFile.is_open())
         {
-            break;
+            std::cout << "Error when opening config file " << configFileName << " Does the file exist? " << "'" << configFile.eof() << "'" << std::endl;
+            return false;
         }
 
-        std::cout << "The grid size is " << gridSizeSquare << " x " << gridSizeSquare << std::endl;
+        while (configFile)
+        {
+            std::string line;
+            std::getline(configFile, line, ':');
+
+            configFile >> gridSizeSquare;
+
+            configFile >> std::ws;
+
+            if (!configFile)
+            {
+                break;
+            }
+
+            std::cout << "The grid size is " << gridSizeSquare << " x " << gridSizeSquare << std::endl;
+        }
+        configFile.close();
+        return true;
     }
-    configFile.close();
+    else
+    {
+        gridSizeSquare = 10;
+        return true;
+    }
+}
+
+
+void elementryCA()
+{
+    for (int i = 0; i < col; i++)
+    {
+        std::cout << std::endl;
+        for (int j = 0; j < row; j++)
+        {
+            grid[i][j] = rand() % 2;
+            std::cout << grid[i][j];
+
+        }
+        
+    }
+}
+
+void drawCA()
+{
+    GLdouble x = -1;
+    GLdouble y = 1;
+
+    GLdouble yPos = 1.0 / row;
+    GLdouble xPos = 1.0 / col;
+
+    for (int i = 0; i < col; i++)
+    {
+        
+        x += xPos;
+        y = 1;
+        //std::cout << std::endl;
+        for (int j = 0; j < row; j++)
+        {
+            
+            
+
+            if (grid[i][j] == 1)
+            {
+                glColor3f(1, 1, 1);
+            }
+            else
+            {
+                glColor3f(0, 0, 0);
+            }
+            //counter clockwise
+            glBegin(GL_TRIANGLES);
+            glVertex3f(x, y, 0);
+            glVertex3f(x, y - yPos, 0);
+            glVertex3f(x + xPos, y - yPos, 0);
+
+            glVertex3f(x, y, 0);
+            glVertex3f(x + xPos, y - yPos, 0);
+            glVertex3f(x + xPos, y, 0);
+            glEnd();
+            y -= yPos;
+        }
+        
+
+    }
+    
+    
+}
+
+void generatePixels(int xOff, int yOff, int width, int height)
+{
+    int offset = 0;
+    for (int r = row - 1; r >= 0; r--) {
+        for (int c = 0; c < col; c++) {
+            image_buffer[col * offset + c] = grid[c][r];
+            LOG("image buffer: ", image_buffer[col * offset + c]);
+        }
+        offset++;
+    }
 }
 
